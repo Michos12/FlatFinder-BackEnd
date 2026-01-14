6cd0f6580abdbde2bb9b06ba499e0d0c0e6ad836
Asuka-Fukuchi <afukuchi24@gmail.com>
2026-01-14 10:53:54 -0800

    Updated name of arg in flatservice

--- services/flat.service.js
+++ services/flat.service.js
@@ -5,26 +5,26 @@ export async function getAllFlatsService() {
     return flats;
 }
 
-export async function updateFlatService(data, id) {
-    const updatedFlat = await Flat.findByIdAndUpdate(id, data, {
+export async function updateFlatService(flatData, flatId) {
+    const updatedFlat = await Flat.findByIdAndUpdate(flatId, flatData, {
         new: true,
         runValidators: true
     });
     return updatedFlat;
 }
 
-export async function deleteFlatService(id) {
-    const deletedFlat = await Flat.findByIdAndDelete(id);
+export async function deleteFlatService(flatId) {
+    const deletedFlat = await Flat.findByIdAndDelete(flatId);
     return deletedFlat;
 }
 
-export async function addFlatService(data) {
-    const newFlat = new Flat(data);
+export async function addFlatService(flatData) {
+    const newFlat = new Flat(flatData);
     await newFlat.save();
     return newFlat;
 }
 
-export async function getFlatByIdService(id) {
-    const flat = await Flat.findById(id);
+export async function getFlatByIdService(flatId) {
+    const flat = await Flat.findById(flatId);
     return flat;
 }

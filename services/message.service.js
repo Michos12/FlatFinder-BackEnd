614c4e01f80c2a27cb0d7e7c2468d9e51714a2e8
Asuka-Fukuchi <afukuchi24@gmail.com>
2026-01-14 11:08:35 -0800

    Created message service

---
+++ services/message.service.js
@@ -0,0 +1,17 @@
+import { Message } from "../models/message.model";
+
+export async function getAllMessagesService(flatId) {
+    const messages = await Message.find({ flatId });
+    return messages;
+}
+
+export async function getUserMessagesService(flatId, senderId) {
+    const messages = await Message.find({ flatId, senderId });
+    return messages;
+}
+
+export async function addMessageService(messageData) {
+    const newMessage = new Message(messageData);
+    await newMessage.save();
+    return newMessage;
+}

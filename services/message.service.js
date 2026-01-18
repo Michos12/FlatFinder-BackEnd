d097fcce62e6d3abe12ea059a875db8ffec7f358
Asuka-Fukuchi <afukuchi24@gmail.com>
2026-01-18 15:32:02 -0800

    added sort method into message service

--- services/message.service.js
+++ services/message.service.js
@@ -1,12 +1,12 @@
 import { Message } from "../models/message.model.js";
 
 export async function getAllMessagesService(flatId) {
-    const messages = await Message.find({ flatId });
+    const messages = await Message.find({ flatId }).sort({ createdAt: 1 });
     return messages;
 }
 
 export async function getUserMessagesService(flatId, senderId) {
-    const messages = await Message.find({ flatId, senderId });
+    const messages = await Message.find({ flatId, senderId }).sort({ createdAt: 1 });
     return messages;
 }
 

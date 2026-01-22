a7e609cac98f5db9d7b71e7a5206d1453a695825
Michael Veliz <129358905+Michos12@users.noreply.github.com>
2026-01-22 00:43:19 -0800

    Update authAdmin.js

--- middleware/authAdmin.js
+++ middleware/authAdmin.js
@@ -1,17 +1,28 @@
-function admin(req, res, next, permissionNeeded) {
+function admin(permissionNeeded = "admin", req = req, res = res) {
     const token = req.header('x-auth-token');
     if(!token) return false
     try {
         const decoded = jwt.verify(token, process.env.SECRET_KEY);
         req.user = decoded;
-        if(req.user.role == 'admin'){
-            return true
-        } else if(permissionNeeded == "owner"){
-            if (req.user.role == "owner") return true
-            else return return false; 
-        } else {
-            return return false;
-        }
+            if(req.user.role == "admin") return true
+            switch(permissionNeeded){
+                case "owner":
+                    if(req.user.role == "owner" || req.user.role == "admin"){
+                        return true
+                        break
+                    } else {
+                        return false
+                        break
+                    }
+                case "admin":
+                    if(req.user.role == "admin"){
+                        return true
+                        break
+                    } else {
+                        return false
+                        break
+                    }
+            }
     } catch (err) {
         console.error('error: ' + err });
         return false

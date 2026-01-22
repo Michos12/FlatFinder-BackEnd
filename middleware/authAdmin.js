fac5b0c979522f778bf305f3f7d15f700778ca02
Michael Veliz <129358905+Michos12@users.noreply.github.com>
2026-01-21 21:08:26 -0800

    Update authAdmin.js

--- middleware/authAdmin.js
+++ middleware/authAdmin.js
@@ -1,19 +1,20 @@
-function admin(req, res, next) {
+function admin(req, res, next, permissionNeeded) {
     const token = req.header('x-auth-token');
-    if(!token) return res.status(401).json({ error: "Access denied. No token provided."})
+    if(!token) return false
     try {
         const decoded = jwt.verify(token, process.env.SECRET_KEY);
         req.user = decoded;
         if(req.user.role == 'admin'){
-            next()
-        } else if(req.user.role == "owner"){
-            if (req.user.role == "owner") next()
-            else return res.status(403).json({ error: "Access denied. You can only modify your own account." }); 
+            return true
+        } else if(permissionNeeded == "owner"){
+            if (req.user.role == "owner") return true
+            else return return false; 
         } else {
-            return res.status(403).json({ error: "Access denied. Admins and accounts owner only can access this." });
+            return return false;
         }
     } catch (err) {
-        res.status(400).json({ error: err.message });
+        console.error('error: ' + err });
+        return false
     }
 };
 

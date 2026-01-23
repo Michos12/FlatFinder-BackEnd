2e6a91b3b4d9ddc4fb835e31abb323164afd5161
Michael_Veliz <corozomichel@gmail.com>
2026-01-22 18:20:56 -0800

    Fusion of branches real final

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -3,18 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-function verifyToken() {
-    return (req, res, next) => {
-        console.log(req)
-        const token = req.header('x-auth-token');
-        if(!token) return res.status(401).json({ error: "Access denied. No token provided."})
-        try {
-            const decoded = jwt.verify(token, process.env.SECRET_KEY);
-            req.user = decoded;
-            next();
-        } catch (err) {
-            res.status(400).json({ error: err.message });
-        }
+function verifyToken(req, res, next) {
+    const token = req.header('x-auth-token');
+    if(!token) return res.status(401).json({ error: "Access denied. No token provided."})
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        res.status(400).json({ error: err.message });
     }
 }
 
